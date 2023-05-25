3ff8f454c8c949a3e697ca78239496077b0ca53d
mateusz-kiszczak <kiszczak.m@gmail.com>
2023-05-25 18:16:55 +0100

    form-validation.php add regular expression validation for form input

--- php/form-validation.php
+++ php/form-validation.php
@@ -39,13 +39,20 @@ $message = '';
 // Firrst name, Surname.
 function is_text($text, $min = 0, $max = 100) {
   $length = mb_strlen($text);
-  return ($length >= $min and $length <= $max);
+  if  ( preg_match('/^[a-zA-Z\-\'\s]{2,64}$/', $text)
+        and preg_match('/^([^0-9]*)$/', $text)) {
+    return ($length >= $min and $length <= $max);
+  } 
+  return false;
 }
 
 // Checks if number fits the range.
 // Age.
-function is_number($number, $min = 0, $max = 999): bool {
-  return ($number >= $min and $number <= $max);
+function is_number($number, $min = 16, $max = 99): bool {
+  if  ( preg_match('/^[1-9]{1,2}$/', $number) ) {
+    return ($number >= $min and $number <= $max);
+  }
+  return false;
 }
 
 // Checks if password meets the requirements (is strong).
@@ -74,7 +81,7 @@ function is_same($val1, $val2): bool {
 // Checks if email fits the pattern.
 // Email.
 function is_email($email): bool {
-  if ( filter_var($email, FILTER_VALIDATE_EMAIL) ) {
+  if ( preg_match('/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/', $email) ) {
     return true;
   }
   return false;
@@ -86,7 +93,7 @@ function is_phone_number($phone, $min = 9, $max = 13) {
   $numLength = strlen((string)$phone);
   $isLength = ($numLength >= $min and $numLength <= $max);
 
-  if ( $isLength and preg_match('/[0-9]/', $phone) ) {
+  if ( $isLength and preg_match('/^[0-9]{9,13}$/', $phone) ) {
     return true;
   }
   return false;
@@ -107,11 +114,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $userInput['contact'] = $_POST['contact'];
   $userInput['terms'] = (isset($_POST['terms']) and $_POST['terms'] == true) ? true : false;
 
-  $errors['name'] = is_text($userInput['name'], 2, 64) ? '' : 'Name must be 2-64 letters.';
+  $errors['name'] = is_text($userInput['name'], 2, 64) ? '' : 'Name must be 2-64 letters and do not contains numbers.';
   $errors['surname'] = is_text($userInput['surname'], 2, 64) ? '' : 'Surname must be 2-64 letters.';
   $errors['age'] = is_number($userInput['age'], 16, 99) ? '' : 'You must be 16-99 years old.';
   $errors['email'] = is_email($userInput['email']) ? '' : 'Your email address is incorect.';
-  $errors['password'] = is_password_strong($userInput['password']) ? '' : 'Your password is not strong enough. Password needs at least one big letter, one number and one of the following special characters: ! £ $ & ? @ . : ; _ -.';
+  $errors['password'] = is_password_strong($userInput['password']) ? '' : nl2br("Your password is not strong enough. \nPasword must be at least 8 characters long. \nPassword needs at least one big letter, one number and one of the following special characters: ! £ $ & ? @ . : ; _ -.");
   $errors['repeatPassword'] = is_same($userInput['password'], $userInput['repeatPassword']) ? '' : 'Password does not match previous one.';
   $errors['phone'] = is_phone_number($userInput['phone']) ? '' : 'Invalid phone number. Please eneter your mobile phone number WITHOUT the prefix.';
   $errors['contact'] = $userInput['contact'] ? '' : 'Please choose preferred contact method.';
