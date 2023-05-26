263df14ac48b328177569b5b42eab6aa3ae73b3d
mateusz-kiszczak <kiszczak.m@gmail.com>
2023-05-26 20:13:27 +0100

    Update and style form-success.php which trigger after valid form submitted

--- php/form-success.php
+++ php/form-success.php
@@ -1,26 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="An example of using form validation on both the front-end side with JavaScript and the back-end side with Php.">
-  <title>Fullstack form validation</title>
-  <link rel="stylesheet" href="../styles/styles.css">
-</head>
-<body>
-<div class="page-layout">
-  <div class="content-wrapper">
-  <?php require_once 'modules/header.php'; ?>
-
+  <?php
+    $results = $_POST;
+  ?>
+  
   <main>
-    <pre>
-      <?php var_dump($_POST); ?>
-    </pre>
-  </main>
+    <section class="form-results">
+      <h2 class="form-results__header">results from your form</h2>
+      <p class="form-results__user-input">Name: <br><span><?= $results['name'] ?></span></p>
+      <p class="form-results__user-input">Surname: <br><span><?= $results['surname'] ?></span></p>
+      <p class="form-results__user-input">Age: <br><span><?= $results['age'] ?></span></p>
+      <p class="form-results__user-input">Email: <br><span><?= $results['email'] ?></span></p>
+      <p class="form-results__user-input">Password: <br><span><?= $results['password'] ?></span></p>
+      <p class="form-results__user-input">Phone number: <br><span><?= $results['phone'] ?></span></p>
+      <p class="form-results__user-input">Preferred contact way: <br><span><?= $results['contact'] ?></span></p>
+      <p class="form-results__user-input"> Favorite programming language:
+        <br>
+        <span>
+          <?= isset($results['favorite-language']) ? $results['favorite-language'] : 'You did not specify your favorite programming language.' ?>
+        </span>
+      </p>
+      <div>
+        <p class="form-results__user-input">Your form rate:
+          <br> 
+          <span>
+            <?= isset($results['rating']) ? '' : 'You did not rate this form' ?>
+          </span>
+        </p>
+        <div class="stars-container">
+          <?php 
+            if (isset($results['rating'])) {
 
-  <?php include 'modules/footer.php'; ?>
-  </div>
-</div>
-</body>
-</html>
+              $stars_length = isset($results['rating']) ? $results['rating'] : '0';
+              $empty_stars_length = 5 - $stars_length;
+              
+              for ($i = 0; $i < $stars_length; $i++) {
+                echo '<div class="star"><img src="./img/gold-star.svg" alt="gold star" /></div>' ;
+              }
+              
+              for ($i = 0; $i < $empty_stars_length; $i++) {
+                echo '<div class="star"><img src="./img/empty-star.svg" alt="empty star" /></div>' ;
+              }
+            }
+          ?>
+        </div>
+      </div>
+    </section>
+  </main>
+  
