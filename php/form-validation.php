fffa038b0eadb3f36f03eeb297eb31ea4d8a56be
mateusz-kiszczak <kiszczak.m@gmail.com>
2023-06-01 23:04:24 +0100

    form-validation.php Add form validation with PHP filters.

--- php/form-validation.php
+++ php/form-validation.php
@@ -18,6 +18,21 @@ $userInput = [
   'terms'             => ''
 ];
 
+// For filters validation.
+$user = [
+  'name'              => '',
+  'surname'           => '',
+  'age'               => '',
+  'email'             => '',
+  'password'          => '',
+  'repeatPassword'    => '',
+  'phone'             => '',
+  'contact'           => '',
+  'favoriteLanguage'  => '',
+  'rating'            => '',
+  'terms'             => ''
+];
+
 $errors = [
   'name'            => '',
   'surname'         => '',
@@ -105,26 +120,43 @@ function is_phone_number($phone, $min = 9, $max = 13) {
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
   // Required fields.
-  $userInput['name'] = $_POST['name'];
-  $userInput['surname'] = $_POST['surname'];
-  $userInput['age'] = $_POST['age'];
-  $userInput['email'] = $_POST['email'];
-  $userInput['password'] = $_POST['password'];
-  $userInput['repeatPassword'] = $_POST['repeat-password'];
-  $userInput['phone'] = $_POST['phone'];
-  $userInput['contact'] = $_POST['contact'];
-  $userInput['terms'] = (isset($_POST['terms']) and $_POST['terms'] == true) ? true : false;
+  $userInput['name']            = $_POST['name'];
+  $userInput['surname']         = $_POST['surname'];
+  $userInput['age']             = $_POST['age'];
+  $userInput['email']           = $_POST['email'];
+  $userInput['password']        = $_POST['password'];
+  $userInput['repeatPassword']  = $_POST['repeat-password'];
+  $userInput['phone']           = $_POST['phone'];
+  $userInput['contact']         = $_POST['contact'];
+  $userInput['terms']           = (isset($_POST['terms']) and $_POST['terms'] == true) ? true : false;
 
   // Errors handeling.
-  $errors['name'] = is_text($userInput['name'], 2, 64) ? '' : 'Name must be 2-64 letters and do not contains numbers.';
-  $errors['surname'] = is_text($userInput['surname'], 2, 64) ? '' : 'Surname must be 2-64 letters.';
-  $errors['age'] = is_number($userInput['age'], 16, 99) ? '' : 'You must be 16-99 years old.';
-  $errors['email'] = is_email($userInput['email']) ? '' : 'Your email address is incorect.';
-  $errors['password'] = is_password_strong($userInput['password']) ? '' : nl2br("Your password is not strong enough. \nPasword must be at least 8 characters long. \nPassword needs at least one big letter, one number and one of the following special characters: ! £ $ & ? @ . : ; _ -.");
+  $errors['name']           = is_text($userInput['name'], 2, 64) ? '' : 'Name must be 2-64 letters and do not contains numbers.';
+  $errors['surname']        = is_text($userInput['surname'], 2, 64) ? '' : 'Surname must be 2-64 letters.';
+  $errors['age']            = is_number($userInput['age'], 16, 99) ? '' : 'You must be 16-99 years old.';
+  $errors['email']          = is_email($userInput['email']) ? '' : 'Your email address is incorect.';
+  $errors['password']       = is_password_strong($userInput['password']) ? '' : nl2br("Your password is not strong enough. \nPasword must be at least 8 characters long. \nPassword needs at least one big letter, one number and one of the following special characters: ! £ $ & ? @ . : ; _ -.");
   $errors['repeatPassword'] = is_same($userInput['password'], $userInput['repeatPassword']) ? '' : 'Password does not match previous one.';
-  $errors['phone'] = is_phone_number($userInput['phone']) ? '' : 'Invalid phone number. Please eneter your mobile phone number WITHOUT the prefix.';
-  $errors['contact'] = $userInput['contact'] ? '' : 'Please choose preferred contact method.';
-  $errors['terms'] = $userInput['terms'] ? '' : 'You must agree to the terms and conditions.';
+  $errors['phone']          = is_phone_number($userInput['phone']) ? '' : 'Invalid phone number. Please eneter your mobile phone number WITHOUT the prefix.';
+  $errors['contact']        = $userInput['contact'] ? '' : 'Please choose preferred contact method.';
+  $errors['terms']          = $userInput['terms'] ? '' : 'You must agree to the terms and conditions.';
+
+  // Validation with filters.
+  $validation_filters['name']['filter']                      = FILTER_VALIDATE_REGEXP;
+  $validation_filters['name']['options']['regexp']           = '/^[a-zA-Z\-\'\s]{2,64}$/';
+  $validation_filters['surname']['filter']                   = FILTER_VALIDATE_REGEXP;
+  $validation_filters['surname']['options']['regexp']        = '/^[a-zA-Z\-\'\s]{2,64}$/';
+  $validation_filters['age']['filter']                       = FILTER_VALIDATE_INT;
+  $validation_filters['age']['options']['min_range']         = 16;
+  $validation_filters['age']['options']['max_range']         = 99;
+  $validation_filters['email']['filter']                     = FILTER_VALIDATE_EMAIL;
+  $validation_filters['password']['filter']                  = FILTER_VALIDATE_REGEXP;
+  $validation_filters['password']['options']['regexp']       = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/';
+  $validation_filters['phone']['filter']                     = FILTER_VALIDATE_REGEXP;
+  $validation_filters['phone']['options']['regexp']          = '/^[0-9]{9,13}$/';
+  $validation_filters['terms']['filter']                     = FILTER_VALIDATE_BOOLEAN;
+
+  $user = filter_input_array(INPUT_POST, $validation_filters);
 
   $invalid = implode($errors);
   if ( $invalid ) {
@@ -132,6 +164,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   } else {
     $form_passed = true;
   }
+
+  // Sanitize data.
+  $userInput['name']    = filter_var($userInput['name'], FILTER_SANITIZE_SPECIAL_CHARS);
+  $userInput['surname'] = filter_var($userInput['surname'], FILTER_SANITIZE_SPECIAL_CHARS);
+  $userInput['age']     = filter_var($userInput['age'], FILTER_SANITIZE_NUMBER_INT);
+  $userInput['email']   = filter_var($userInput['email'], FILTER_SANITIZE_EMAIL);
+  $userInput['phone']   = filter_var($userInput['phone'], FILTER_SANITIZE_NUMBER_INT);
 }
 
 ?>
